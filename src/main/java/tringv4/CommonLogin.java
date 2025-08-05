315cb55b526ea48f5cc616931a53993f543cd9da
naveen23042000 <naveen.v2304@gmail.com>
2025-08-05 15:34:59 +0530

    Update CommonLogin.java

--- src/main/java/tringv4/CommonLogin.java
+++ src/main/java/tringv4/CommonLogin.java
@@ -1,34 +1,16 @@
 package tringv4;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.*;
-
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CommonLogin {
 
     public static WebDriver driver;
     public static WebDriverWait wait;
 
-    public static void loginToTringApp(String email, String password) {
-        // ChromeOptions
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-
-        Map<String, Object> prefs = new HashMap<>();
-        prefs.put("credentials_enable_service", false);
-        prefs.put("profile.password_manager_enabled", false);
-        prefs.put("autofill.profile_enabled", false);
-        options.setExperimentalOption("prefs", prefs);
-
-        // Driver + Wait
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    public static void loginToTringApp(String email, String password, WebDriver webDriver, WebDriverWait webWait) {
+        driver = webDriver;
+        wait = webWait;
 
         try {
             // Step 1: Go to Login Page
